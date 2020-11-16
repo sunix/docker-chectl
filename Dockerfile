425ec59d696001749385d455f5b3e502e634e64f
Sun Tan <sutan@redhat.com>
2020-11-16 10:07:58 +0000

    adding script for arbitrary users
    Signed-off-by: Sun Tan <sutan@redhat.com>

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,17 @@
 FROM centos
+# Set permissions on /etc/passwd and /home to allow arbitrary users to write
+COPY --chown=0:0 entrypoint.sh /
+RUN mkdir -p /home/user && chgrp -R 0 /home && chmod -R g=u /etc/passwd /etc/group /home && chmod +x /entrypoint.sh
+
+# Install chectl
 RUN curl -sL  https://www.eclipse.org/che/chectl/ | bash
+
+# Install common terminal editors in container to aid development process
+COPY install-editor-tooling.sh /tmp
+RUN /tmp/install-editor-tooling.sh && rm -f /tmp/install-editor-tooling.sh
+
+USER 10001
+ENV HOME=/home/user
+WORKDIR /projects
+ENTRYPOINT [ "/entrypoint.sh" ]
+CMD ["tail", "-f", "/dev/null"]
---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+# Ensure $HOME exists when starting
+if [ ! -d "${HOME}" ]; then
+  mkdir -p "${HOME}"
+fi
+
+# Setup $PS1 for a consistent and reasonable prompt
+if [ -w "${HOME}" ] && [ ! -f "${HOME}"/.bashrc ]; then
+  echo "PS1='\[\e[1;31m\]\u\[\e[0;39m\]@\[\e[1;36m\][\$CHE_MACHINE_NAME]\[\e[0;39m\]:\[\e[1;33m\]\w\[\e[0;39m\]\[\e[1;32m\]$(__git_ps1 " (%s)")\[\e[0;39m\] \[\e[1;39m\]\n$ '" > "${HOME}"/.bashrc
+fi
+
+# Add current (arbitrary) user to /etc/passwd and /etc/group
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
+    echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
+  fi
+fi
+
+exec "$@"
+
